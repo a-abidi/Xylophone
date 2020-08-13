af63fdd52e64d7b316d93ae3f720c007ba487db2
a-abidi <aliabidi@icloud.com>
2020-08-13 19:38:01 +0100

    Cleaned up code.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,24 +1,10 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
-    var player: AVAudioPlayer?
-    
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
-        player = try! AVAudioPlayer(contentsOf: url!)
-        player?.play()
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -27,7 +13,10 @@ class ViewController: UIViewController {
         playSound()
     }
     
-    
-
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
