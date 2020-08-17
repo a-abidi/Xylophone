f479182d12cad06252051a46857f0295399d47fc
a-abidi <aliabidi@icloud.com>
2020-08-17 16:39:06 +0100

    Keys decrease in opacity for 0.2 seconds when pressed.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -11,12 +11,16 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         playSound(soundName: sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+        //Bring's sender's opacity back up to fully opaque.
+        sender.alpha = 1.0
+        }
     }
     
-    func playSound(soundName: String) { 
+    func playSound(soundName: String) {
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
-                
     }
 }
